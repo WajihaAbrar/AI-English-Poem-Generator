7398ae1ca79b86020c6718fc293172df4d166ccd
WajihaAbrar <wajihaabrar@gmail.com>
2023-12-13 12:37:41 +0000

    Added the basic form handling and typewriter plugin.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Flowers are beautiful",
+    autoStart: true,
+    delay: 50,
+    cursor: null,
+  });
+}
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
